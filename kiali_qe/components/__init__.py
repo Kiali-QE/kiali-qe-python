2e57099ba903fd60a7ab0fdd68b00e3fb3b35684
Hayk Hovsepyan <hhovsepy@redhat.com>
2019-05-16 11:27:39 +0200

    FF specific fixes

--- kiali_qe/components/__init__.py
+++ kiali_qe/components/__init__.py
@@ -2,6 +2,7 @@
 import re
 
 from widgetastic.widget import Checkbox, TextInput, Widget
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from kiali_qe.components.enums import (
     HelpMenuEnum,
@@ -206,7 +207,7 @@ class Notification(Widget):
 
 
 class DropDown(Widget):
-    ROOT = '//*[contains(@class, "input-group")]/*[contains(@class, "dropdown")]/../..'
+    ROOT = '//*[contains(@class, "input-group")]/div[contains(@class, "dropdown")]/../..'
     SELECT_BUTTON = './/*[contains(@class, "dropdown-toggle")]'
     OPTIONS_LIST = ('.//*[contains(@class, "dropdown-menu")]'
                     '//*[not(contains(@class, "disabled"))]//*[contains(@role, "menuitem")]')
@@ -421,9 +422,9 @@ class FilterList(Widget):
 
 class Filter(Widget):
     ROOT = '//*[contains(@class, "toolbar-pf-actions")]//*[contains(@class, "toolbar-pf-filter")]'
-    FILTER_DROPDOWN = '//*[contains(@class, "dropdown")]'
+    FILTER_DROPDOWN = '//div[contains(@class, "dropdown")]'
     VALUE_INPUT = './/input'
-    VALUE_DROPDOWN = './/*[contains(@class, "filter-pf-select")]//*[contains(@class, "dropdown")]'
+    VALUE_DROPDOWN = './/*[contains(@class, "filter-pf-select")]//div[contains(@class, "dropdown")]'
 
     def __init__(self, parent, locator=None, logger=None):
         Widget.__init__(self, parent, logger=logger)
@@ -458,6 +459,8 @@ class Filter(Widget):
         if len(self.browser.elements(parent=self, locator=self.VALUE_INPUT)):
             _input = TextInput(parent=self, locator=self.VALUE_INPUT)
             _input.fill(value + '\n')
+            if self.browser.browser_type == 'firefox':
+                self.browser.send_keys(Keys.ENTER, _input)
         elif len(self.browser.elements(parent=self, locator=self.VALUE_DROPDOWN)):
             _dropdown = DropDown(parent=self, locator=self.VALUE_DROPDOWN)
             _dropdown.select(value)
@@ -853,6 +856,8 @@ class Pagination(Widget):
 
     def move_to_page(self, page_number):
         self._page_input.fill('{}\n'.format(page_number))
+        if self.browser.browser_type == 'firefox':
+            self.browser.send_keys(Keys.ENTER, self._page_input)
         wait_to_spinner_disappear(self.browser)
 
     @property
