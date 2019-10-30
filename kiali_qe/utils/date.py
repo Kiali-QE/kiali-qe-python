cd8d4fef3eda769b3cda7a7cd736cbca5dc13c55
Filip Brychta <fbrychta@redhat.com>
2019-10-30 10:03:24 +0100

    Get new date format in PF4

--- kiali_qe/utils/date.py
+++ kiali_qe/utils/date.py
@@ -3,21 +3,24 @@ from dateutil.tz import tzlocal, tzutc
 
 REST_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
-UI_FORMAT = '%m/%d/%Y, %I:%M:%S %p'
+UI_FORMAT = '%b %d, %I:%M %p'
 
 
 def parse_from_ui(date_str):
     if (date_str == '-') or (date_str == ''):
         return None
     else:
-        return datetime.strptime(date_str, UI_FORMAT).replace(tzinfo=tzlocal()).astimezone(tzutc())
+        # TODO: SWSQE-989
+        return datetime.strptime(date_str, UI_FORMAT).\
+            replace(year=datetime.now().year, tzinfo=tzlocal()).astimezone(tzutc())
 
 
 def parse_from_rest(date_str):
     if (date_str == '-') or (date_str == ''):
         return None
     else:
-        return datetime.strptime(date_str, REST_FORMAT).replace(tzinfo=tzutc())
+        # TODO: SWSQE-989
+        return datetime.strptime(date_str, REST_FORMAT).replace(second=0, tzinfo=tzutc())
 
 
 def from_rest_to_ui(date_str):
