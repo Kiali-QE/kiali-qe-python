33e608e9e7451d117b137726c126165408678649
Hayk Hovsepyan <hhovsepy@redhat.com>
2020-05-05 13:23:37 +0200

    Added new test for verification advanced settings keeping.

--- kiali_qe/tests/__init__.py
+++ kiali_qe/tests/__init__.py
@@ -1220,7 +1220,8 @@ class ServicesPageTest(AbstractListPageTest):
     def test_routing_create(self, name, namespace, routing_type,
                             tls=RoutingWizardTLS.ISTIO_MUTUAL, load_balancer=True,
                             load_balancer_type=RoutingWizardLoadBalancer.ROUND_ROBIN,
-                            gateway=True, include_mesh_gateway=True):
+                            gateway=True, include_mesh_gateway=True,
+                            skip_advanced=False):
         logger.debug('Routing Wizard {} for Service: {}, {}'.format(routing_type, name, namespace))
         # load service details page
         self._prepare_load_details_page(name, namespace)
@@ -1230,7 +1231,8 @@ class ServicesPageTest(AbstractListPageTest):
             assert self.page.actions.create_weighted_routing(
                 tls=tls, load_balancer=load_balancer,
                 load_balancer_type=load_balancer_type, gateway=gateway,
-                include_mesh_gateway=include_mesh_gateway)
+                include_mesh_gateway=include_mesh_gateway,
+                skip_advanced=skip_advanced)
             assert not self.page.actions.is_delete_disabled()
             assert self.page.actions.is_update_weighted_enabled()
             assert self.page.actions.is_create_matching_disabled()
@@ -1239,7 +1241,8 @@ class ServicesPageTest(AbstractListPageTest):
             assert self.page.actions.create_matching_routing(
                 tls=tls, load_balancer=load_balancer,
                 load_balancer_type=load_balancer_type, gateway=gateway,
-                include_mesh_gateway=include_mesh_gateway)
+                include_mesh_gateway=include_mesh_gateway,
+                skip_advanced=skip_advanced)
             assert not self.page.actions.is_delete_disabled()
             assert self.page.actions.is_update_matching_enabled()
             assert self.page.actions.is_create_weighted_disabled()
@@ -1248,7 +1251,8 @@ class ServicesPageTest(AbstractListPageTest):
             assert self.page.actions.suspend_traffic(
                 tls=tls, load_balancer=load_balancer,
                 load_balancer_type=load_balancer_type, gateway=gateway,
-                include_mesh_gateway=include_mesh_gateway)
+                include_mesh_gateway=include_mesh_gateway,
+                skip_advanced=skip_advanced)
             assert not self.page.actions.is_delete_disabled()
             assert self.page.actions.is_create_matching_disabled()
             assert self.page.actions.is_create_weighted_disabled()
@@ -1294,7 +1298,8 @@ class ServicesPageTest(AbstractListPageTest):
     def test_routing_update(self, name, namespace, routing_type,
                             tls=RoutingWizardTLS.ISTIO_MUTUAL, load_balancer=True,
                             load_balancer_type=RoutingWizardLoadBalancer.ROUND_ROBIN,
-                            gateway=True, include_mesh_gateway=True):
+                            gateway=True, include_mesh_gateway=True,
+                            skip_advanced=False):
         logger.debug('Routing Update Wizard {} for Service: {}, {}'.format(routing_type,
                                                                            name,
                                                                            namespace))
@@ -1305,7 +1310,8 @@ class ServicesPageTest(AbstractListPageTest):
             assert self.page.actions.update_weighted_routing(
                 tls=tls, load_balancer=load_balancer,
                 load_balancer_type=load_balancer_type, gateway=gateway,
-                include_mesh_gateway=include_mesh_gateway)
+                include_mesh_gateway=include_mesh_gateway,
+                skip_advanced=skip_advanced)
             assert not self.page.actions.is_delete_disabled()
             assert self.page.actions.is_update_weighted_enabled()
             assert self.page.actions.is_create_matching_disabled()
@@ -1314,7 +1320,8 @@ class ServicesPageTest(AbstractListPageTest):
             assert self.page.actions.update_matching_routing(
                 tls=tls, load_balancer=load_balancer,
                 load_balancer_type=load_balancer_type, gateway=gateway,
-                include_mesh_gateway=include_mesh_gateway)
+                include_mesh_gateway=include_mesh_gateway,
+                skip_advanced=skip_advanced)
             assert not self.page.actions.is_delete_disabled()
             assert self.page.actions.is_update_matching_enabled()
             assert self.page.actions.is_create_weighted_disabled()
@@ -1323,7 +1330,8 @@ class ServicesPageTest(AbstractListPageTest):
             assert self.page.actions.update_suspended_traffic(
                 tls=tls, load_balancer=load_balancer,
                 load_balancer_type=load_balancer_type, gateway=gateway,
-                include_mesh_gateway=include_mesh_gateway)
+                include_mesh_gateway=include_mesh_gateway,
+                skip_advanced=skip_advanced)
             assert not self.page.actions.is_delete_disabled()
             assert self.page.actions.is_create_matching_disabled()
             assert self.page.actions.is_create_weighted_disabled()
--- kiali_qe/tests/test_routing_wizard.py
+++ kiali_qe/tests/test_routing_wizard.py
@@ -4,7 +4,8 @@ from kiali_qe.tests import ServicesPageTest
 from kiali_qe.components.enums import (
     RoutingWizardType,
     RoutingWizardLoadBalancer,
-    RoutingWizardTLS
+    RoutingWizardTLS,
+    IstioConfigObjectType
 )
 
 BOOKINFO_2 = 'bookinfo2'
@@ -36,6 +37,41 @@ def test_weighted_routing_single(kiali_client, openshift_client, browser, pick_n
                               tls=RoutingWizardTLS.SIMPLE, load_balancer=True,
                               load_balancer_type=RoutingWizardLoadBalancer.LEAST_CONN,
                               gateway=False, include_mesh_gateway=False)
+    tests.test_routing_delete(name=name, namespace=namespace)
+
+
+@pytest.mark.p_ro_namespace
+@pytest.mark.p_crud_group6
+def test_routing_keep_advanced_settings(kiali_client, openshift_client, browser, pick_namespace):
+    tests = ServicesPageTest(
+        kiali_client=kiali_client, openshift_client=openshift_client, browser=browser)
+    # use only bookinfo2 namespace where colliding tests are in the same p_group
+    namespace = pick_namespace(BOOKINFO_2)
+    name = 'details'
+    tests.test_routing_create(name=name, namespace=namespace,
+                              routing_type=RoutingWizardType.CREATE_WEIGHTED_ROUTING,
+                              tls=RoutingWizardTLS.ISTIO_MUTUAL, load_balancer=True,
+                              load_balancer_type=RoutingWizardLoadBalancer.ROUND_ROBIN,
+                              gateway=True, include_mesh_gateway=True)
+    tests.test_routing_update(name=name, namespace=namespace,
+                              routing_type=RoutingWizardType.UPDATE_WEIGHTED_ROUTING,
+                              skip_advanced=True)
+    service_details = kiali_client.service_details(service_name=name, namespace=namespace)
+    assert len(service_details.virtual_services) == 1
+    vs_details = kiali_client.istio_config_details(
+        namespace=namespace,
+        object_type=IstioConfigObjectType.VIRTUAL_SERVICE.text,
+        object_name=service_details.virtual_services[0].name)
+    assert '\"gateways\": [\"bookinfo/bookinfo-gateway\", \"mesh\"]' in vs_details.text
+    assert len(service_details.destination_rules) == 1
+    dr_details = kiali_client.istio_config_details(
+        namespace=namespace,
+        object_type=IstioConfigObjectType.DESTINATION_RULE.text,
+        object_name=service_details.destination_rules[0].name)
+    assert '\"simple\": \"{}\"'.format(
+        RoutingWizardLoadBalancer.ROUND_ROBIN.text) in dr_details.text
+    assert '\"mode\": \"{}\"'.format(
+        RoutingWizardTLS.ISTIO_MUTUAL.text) in dr_details.text
     tests.test_routing_delete(name=name, namespace=namespace)
 
 
