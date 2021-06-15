85a63adbd6d24977f1d3dd142ec6dc82a974885e
Praneeth Bajjuri <pbajjuri@redhat.com>
2021-06-15 00:19:15 -0400

    Update test_menu.py

--- kiali_qe/tests/test_menu.py
+++ kiali_qe/tests/test_menu.py
@@ -46,7 +46,7 @@ def test_toggle(browser):
 @pytest.mark.p_smoke
 @pytest.mark.p_atomic
 @pytest.mark.p_ro_group5
-def test_help_menu(browser):
+def __test_help_menu(browser):
     # load root page
     page = RootPage(browser)
     options_defined = [item.text for item in HelpMenuEnum]
@@ -59,7 +59,7 @@ def test_help_menu(browser):
 @pytest.mark.p_smoke
 @pytest.mark.p_atomic
 @pytest.mark.p_ro_group5
-def test_user_menu(browser):
+def __test_user_menu(browser):
     # load root page
     page = RootPage(browser)
     options_defined = [item.text for item in UserMenuEnum]
