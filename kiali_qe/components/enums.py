05f1d951196c6461e9c1cdb6d55aaf3d58921918
pbajjuri20 <pbajjuri@redhat.com>
2021-08-23 17:50:45 -0400

    update_metrics_time_interval

--- kiali_qe/components/enums.py
+++ kiali_qe/components/enums.py
@@ -73,10 +73,6 @@ class MetricsTimeInterval(StringEnum):
     LAST_HOUR = ('Last 1h')
     LAST_3_HOURS = ('Last 3h')
     LAST_6_HOURS = ('Last 6h')
-    LAST_12_HOURS = ('Last 12h')
-    LAST_1_DAY = ('Last 1d')
-    LAST_7_DAYS = ('Last 7d')
-    LAST_30_DAYS = ('Last 30d')
     CUSTOM = ('Custom')
 
 
