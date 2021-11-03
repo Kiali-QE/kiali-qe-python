550800c10bd36d0a6194ebdf30754ee89a7a36e9
pbajjuri20 <pbajjuri@redhat.com>
2021-11-03 17:07:24 -0400

    fix Graph Time values

--- kiali_qe/components/enums.py
+++ kiali_qe/components/enums.py
@@ -67,7 +67,6 @@ class TimeIntervalUIText(StringEnum):
     LAST_1_DAY = ('Last 1d')
     LAST_7_DAYS = ('Last 7d')
     LAST_30_DAYS = ('Last 30d')
-    
 
 
 class MetricsTimeInterval(StringEnum):
